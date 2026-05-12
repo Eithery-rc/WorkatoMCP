ff42908bfba72eb32900becbb7e9c9adaaeef2e0
Roman Chikalenko <rchikalenko@blubanyan.com>
2026-05-12 14:34:41 +0300

    feat: register v1.1 discovery tools in extension barrel

--- app/chrome-extension/entrypoints/background/tools/workato/index.ts
+++ app/chrome-extension/entrypoints/background/tools/workato/index.ts
@@ -1,2 +1,6 @@
 export { workatoPullRecipeTool } from './pull-recipe';
 export { workatoJobTraceTool } from './job-trace';
+export { workatoSearchRecipesTool } from './search-recipes';
+export { workatoSearchConnectionsTool } from './search-connections';
+export { workatoGetConnectionTool } from './get-connection';
+export { workatoListJobsTool } from './list-jobs';
